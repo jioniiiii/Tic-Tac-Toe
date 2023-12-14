90b4f8cd0b3e660b412879f2489fc2ed28e87a56
jioniiiii <lpolpo13.group@gmail.com>
2023-12-14 18:54:13 +0200

    needs work

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Tic-Tac-Toe
-    -add ui 
-    -add buttons(start game,play again)
+    -add ui
+    -fix playing game function(make one function to handle eventlisteners and one to playthe game so they can be removed after the end of hte round)
+    -add user names to the players and to pick witch marker they want
+    
     -mayabe "ai"
--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 //for all the dom 
 const DOM = (function(){
     const boardCont = document.querySelector(".cont");
+    const btnRestart = document.querySelector(".btn-restart");
 
     function getBoardCont(){
         return boardCont;
@@ -13,9 +14,20 @@ const DOM = (function(){
         return square;
     }
 
+    function reset(){
+        btnRestart.addEventListener('click', function(){
+            gameBoard.resetBoard();
+            control.resetPlayerTrun();
+            control.playingGame();
+            console.log("clear");
+        })
+    }
+
+    reset();
+
     return {
         getBoardCont: getBoardCont,
-        newSquare: newSquare
+        newSquare: newSquare,
     };
 })();
 
@@ -32,26 +44,43 @@ const gameBoard = (function(){
         }
     }
 
+    function resetBoard(){
+        const squares = boardCont.querySelectorAll(".square");
+        squares.forEach(square => {
+            square.innerHTML = ""; 
+        })
+        //reset the array also
+        for(let i = 0; i < board.length; i++){
+            board[i] = undefined;
+        }
+    }
+
     initBoard();
 
     return{
         getBoard: function(){
             return board;
-        }
+        },
+        resetBoard: resetBoard
     };
 })();
 
 //controling game and playing game
 const control = (function(){
-    player1 = {
+    const player1 = {
         name: "Player 1",
         marker: "X",
     };
     
-    player2 = {
+    const player2 = {
         name: "Player 2",
         marker: "O",
     };
+
+    let playerTurn = true;
+    const gameBoardArray = gameBoard.getBoard();
+    const boardCont = DOM.getBoardCont();
+    let squares = boardCont.querySelectorAll(".square");
 
     function checkWinner(board,player){
         const winConditions = [
@@ -78,13 +107,7 @@ const control = (function(){
         return !board.includes(undefined);//if no empty array tie
     }
 
-    let playerTurn = true;//togle for logic 
-    
     function playingGame() {
-        const boardCont = DOM.getBoardCont();
-        const squares = boardCont.querySelectorAll('.square');
-        const gameBoardArray = gameBoard.getBoard();
-
         squares.forEach((square, index) => {
             square.addEventListener('click', function handleSquareClick(){
                 if (!gameBoardArray[index]) {
@@ -93,27 +116,40 @@ const control = (function(){
                     square.appendChild(marker);
                     
                     gameBoardArray[index] = playerTurn ? player1.marker : player2.marker;
-
+                    
                     if(checkWinner(gameBoardArray,player1.marker)){
-                        alert(`${player1.name} wins!`);
+                        removeEventListener();
+                        console.log(`${player1.name} wins!`);
+                        
                     }
                     else if(checkWinner(gameBoardArray,player2.marker)){
                         alert(`${player2.name} wins!`);
+                        removeEventListener();
                     }
                     else if(checkTie(gameBoardArray)){
-                        alert("tie!")
+                        alert("tie!");
+                        removeEventListener();
                     }
                     else{
                         console.log("gmae still going");
                     }
+                    playerTurn = !playerTurn;
 
+                    function removeEventListener(){
+                        squares.forEach((square) => {
+                            square.removeEventListener('click', handleSquareClick);
+                        })
+                    }
                 }
-
-                playerTurn = !playerTurn;
-                square.removeEventListener('click', handleSquareClick);
             });
         });
     }
 
     playingGame();
+    return{
+        resetPlayerTrun: function(){
+            playerTurn = true;
+        },
+        playingGame: playingGame
+    };
 })();
